a046db51f12f165dace2464ad0aee6a27fab3b92
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-01-24 17:30:48 +0100

    Include more reducers

--- src/components/PizzaList.js
+++ src/components/PizzaList.js
@@ -4,11 +4,11 @@ function PizzaList(props) {
     return (<div>
         <h1>Pizza Explorer</h1>
         <ul>
-            { props.pizzas.map(pizza =>
+            {props.pizzas.map(pizza =>
                 <li key={pizza.id} onClick={() => props.selectPizza(pizza.id)}>
-                { pizza.name }
+                    {pizza.name}
                 </li>
-                )}
+            )}
         </ul>
     </div>)
 }
--- src/index.js
+++ src/index.js
@@ -4,11 +4,11 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 import store from './store'
-import {Provider} from 'react-redux'
+import { Provider } from 'react-redux'
 
 ReactDOM.render(
     <Provider store={store}>
-    <App />
+        <App />
     </Provider>, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
