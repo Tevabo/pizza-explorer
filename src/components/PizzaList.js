34f8b1332212e4d8f0523ab2b94be20879521449
Teun van Boxtel <teunvboxtel@hotmail.nl>
2019-01-24 16:02:02 +0100

    Add basic reducer

---
+++ src/components/PizzaList.js
@@ -0,0 +1,17 @@
+import * as React from 'react'
+
+function PizzaList(props) {
+    return (<div>
+        <h1>Pizza Explorer</h1>
+        <ul>
+            { props.pizzas.map(pizza =>
+                <li key={pizza.id} onClick={() => props.selectPizza(pizza.id)}>
+                { pizza.name }
+                </li>
+                )}
+        </ul>
+    </div>)
+}
+
+
+export default PizzaList
